a8b9c89f9cc3cb517919d96e9f49a4dce040b7a5
purev <purevbayar@gerege.mn>
2022-08-17 19:03:42 +0800

    s

--- pkg/controller/product.go
+++ pkg/controller/product.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	// "net/http"
+	"net/http"
 	"go/pkg/services"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +9,18 @@ import (
 // ProductController : Product controller
 type ProductController struct{}
 
-func (c *ProductController) ProductList(c *gin.Context) {
-	return services.ProductService
+func (*ProductController) ProductList(c *gin.Context) {
+
+	service := services.ProductService{}
+	req, errS := c.GetRawData()
+	reqS := string(req)
+	if errS != nil {
+		resolve(c, reqS, map[string]interface{}{}, map[string]interface{}{"error": errS, "message": "Алдаа"})
+		return
+	}
+	result := service.List(reqS)
+
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.JSON(http.StatusOK, result)
+	c.Request.Context().Done()
 }
--- pkg/services/product.go
+++ pkg/services/product.go
@@ -2,26 +2,13 @@ package services
 
 import (
 	"github.com/gin-gonic/gin"
-	// "go/pkg/db"
+	"net/http"
 
-	// "context"
 )
 
 type ProductService struct{}
 
-func (u *ProductService) List(c *gin.Context) (r *Response) {
-
-	// ctx := context.Background()
-	// tx, _ := db.Tx(ctx)
-
-	// var bank_list []models.AppBanks
-
-	// var query = ""
-	// if req.Name != "" {
-	// 	query += "lower(name) = " + "'" + strings.ToLower(req.Name) + "'"
-	// }
-
-	// tx.Where(query).Order("id asc").Find(&bank_list)
-
-	return r.SuccessWithParams(("success"))
+func (*ProductService) List(c *gin.Context) (r *Response) {
+	c.JSON(http.StatusOK, "")
+	c.Request.Context().Done()
 }
